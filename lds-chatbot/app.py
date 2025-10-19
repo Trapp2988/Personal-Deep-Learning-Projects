895d19727a5e9a8e5c9b3fee362ba8870945a726
Tanner Rapp <rapptanner98@gmail.com>
2025-10-18 18:11:25 -0600

    Add files via upload

--- lds-chatbot/app.py
+++ lds-chatbot/app.py
@@ -11,11 +11,11 @@
 # ### Libraries
 # ---
 
-# In[1]:
+# In[19]:
 
 
 from langchain_community.document_loaders import PyPDFLoader
-from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_huggingface import HuggingFaceEmbeddings
 from langchain_community.vectorstores import FAISS
 from langchain_openai import ChatOpenAI
